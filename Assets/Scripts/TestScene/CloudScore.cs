04c01f8dd1d2d0d313eb77eba72058f08eb1381d
3doriTea <sorasora3program@gmail.com>
2025-10-27 15:16:53 +0900

    スマホ画面の画像追加

--- Assets/Scripts/TestScene/CloudScore.cs
+++ Assets/Scripts/TestScene/CloudScore.cs
@@ -2,24 +2,25 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class CloudScore : MonoBehaviour
+namespace TestScene
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class CloudScore : MonoBehaviour
     {
-        StartCoroutine(GetRequest("https://3dori.yuuronacademy.com"));
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    IEnumerator GetRequest(string url)
-    {
-        using (var request = UnityWebRequest.Get(url))
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
         {
+            StartCoroutine(GetRequest("https://3dori.yuuronacademy.com"));
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        IEnumerator GetRequest(string url)
+        {
+            using var request = UnityWebRequest.Get(url);
             yield return request.SendWebRequest();
 
             if (request.result == UnityWebRequest.Result.ConnectionError
